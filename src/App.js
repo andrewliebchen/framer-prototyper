96525d507496a4e95cb4013fe5b433f3726741d2
Andrew Liebchen <andrewliebchen@gmail.com>
2017-09-19 12:55:55 -0700

    Use dep instead of devDep

--- src/App.js
+++ src/App.js
@@ -27,6 +27,9 @@ const initialCode = {
 const heapCode = `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
 heap.load(3699475241);`;
 
+// eslint-disable-next-line
+const appTitle = "🖼🎉 Framer is fun";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -107,7 +110,7 @@ class App extends Component {
         content={this._renderModalContent()}
       >
         <Helmet>
-          <title>🖼🎉 Framer is fun</title>
+          <title>{appTitle}</title>
           <script type="text/javascript">{heapCode}</script>
         </Helmet>
         <ReactTooltip
